b7d5ba250a3dd31c199708b4fd0f6539fb1c7716
Jeffersonsjsilva <139915402+Jeffersonsjsilva@users.noreply.github.com>
2023-08-31 10:06:31 -0300

    Add files via upload

--- db_cds.sql
+++ db_cds.sql
@@ -261,6 +261,19 @@ tit.cod_cat = cat.cod_cat;
 -- cliente fez.
 
 select
+titp.num_ped as 'Número do pedido',
+cli.nome_cli as 'Nome do cliente',
+tit.nome_cd as 'Titulo'
+from tbClientes as cli
+inner join tbPedidos as ped
+on cli.cod_cli = ped.cod_cli
+inner join tbTitulos_pedidos as titp
+on titp.num_ped = ped.num_ped
+inner join tbTitulos as tit
+on tit.cod_tit = titp.cod_tit
+order by titp.num_ped;
+
+select
 cli.nome_cli as 'Nome do cliente',
 tit.nome_cd as 'CD comprado' from tbClientes as cli
 left join tbTitulos as tit on
